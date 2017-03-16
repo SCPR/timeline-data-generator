b32e9503211f66fe634a0ec29be49a056e6eb02b
Chris Keller <christopherlawrencekeller@gmail.com>
2017-03-16 10:49:47 -0700

    streamlines file a bit more than it originally was and updates to use v3 article api

--- timeline-data-generator.py
+++ timeline-data-generator.py
@@ -6,29 +6,35 @@ import csv
 import datetime
 from datetime import datetime, date
 
+
 logger = logging.getLogger("root")
 logging.basicConfig(
     format = "\033[1;36m%(levelname)s: %(filename)s (def %(funcName)s %(lineno)s): \033[1;37m %(message)s",
     level=logging.DEBUG
 )
 
-def construct_target_url():
-    query_input = raw_input("Enter the search term for the KPCC API: ")
-    results_limit = raw_input(
-        "Enter the number of results you\'d like from the KPCC API: ")
-    query_string = query_input.lower().replace(" ", "+")
-    csv_file_name = query_input.lower().replace(" ", "-")
-    target_url = "http://www.scpr.org/api/v2/content/?types=news,blogs&query=%s&limit=%s" % (
-        query_string, results_limit)
-    open_json_feed(target_url, csv_file_name)
 
-def open_json_feed(target_url, csv_file_name):
-    json_request = urllib2.urlopen(target_url)
+def _init(query, limit):
+    search = query.lower().replace(" ", "+")
+    csv_file_name = query.lower().replace(" ", "-")
+    url = build_search_url(search, limit)
+    json_data = retrieve_json_from(url)
+    create_csv_from(json_data, csv_file_name)
+
+
+def build_search_url(search, limit):
+    target_url = "http://www.scpr.org/api/v3/articles?types=news,blogs&query=%s&limit=%s" % (search, limit)
+    return target_url
+
+
+def retrieve_json_from(url):
+    json_request = urllib2.urlopen(url)
     json_data = json.load(json_request)
-    write_json_as_timeline_data(json_data, csv_file_name)
+    return json_data
 
-def write_json_as_timeline_data(json_data, csv_file_name):
-    with open(csv_file_name + ".csv", "wb+", buffering=0) as newCsvFile:
+
+def create_csv_from(json_data, csv_file_name):
+    with open(csv_file_name + ".csv", "wb+", buffering=0) as new_csv:
         header_row = [
             "title",
             "date",
@@ -40,22 +46,17 @@ def write_json_as_timeline_data(json_data, csv_file_name):
             "read more source",
             "read more url"
         ]
-        dataForCsv = csv.writer(
-            newCsvFile, delimiter=",", quoting=csv.QUOTE_ALL)
-        dataForCsv.writerow(header_row)
-        for data in json_data:
+        csv_object = csv.writer(new_csv, delimiter=",", quoting=csv.QUOTE_ALL)
+        csv_object.writerow(header_row)
+        for data in json_data["articles"]:
             title = data["title"].encode("ascii", "ignore")
-            timeline_date = convert_string_date_for_timeline(
-                data["published_at"], "%b %d, %Y")
-            display_date = convert_string_date_for_timeline(
-                data["published_at"], "%B %d")
+            timeline_date = convert_string_date_for_timeline(data["published_at"], "%b %d, %Y")
+            display_date = convert_string_date_for_timeline(data["published_at"], "%B %d")
             media_type = "image"
             if data["assets"]:
-                media_url = data["assets"][0]["full"][
-                    "url"].encode("ascii", "ignore")
+                media_url = data["assets"][0]["full"]["url"].encode("ascii", "ignore")
                 try:
-                    caption = data["assets"][0][
-                        "caption"].encode("ascii", "ignore")
+                    caption = data["assets"][0]["caption"].encode("ascii", "ignore")
                 except:
                     caption = None
             else:
@@ -63,8 +64,8 @@ def write_json_as_timeline_data(json_data, csv_file_name):
                 caption = None
             teaser = data["teaser"].encode("ascii", "ignore")
             source = "KPCC"
-            url = data["permalink"].encode("ascii", "ignore")
-            dataForCsv.writerow([
+            url = data["public_url"].encode("ascii", "ignore")
+            csv_row = [
                 title,
                 timeline_date,
                 display_date,
@@ -74,8 +75,10 @@ def write_json_as_timeline_data(json_data, csv_file_name):
                 teaser,
                 source,
                 url
-            ])
-        newCsvFile.close()
+            ]
+            logger.debug(csv_row)
+            csv_object.writerow(csv_row)
+
 
 def convert_string_date_for_timeline(input, format):
     string_date = str(input)
@@ -85,5 +88,8 @@ def convert_string_date_for_timeline(input, format):
     output = datetime.strftime(date_object, format)
     return output
 
+
 if __name__ == "__main__":
-    construct_target_url()
+    query_input = raw_input("Enter the search term for the KPCC API: ")
+    results_limit = raw_input("Enter the number of results you\'d like from the KPCC API: ")
+    _init(query_input, results_limit)
